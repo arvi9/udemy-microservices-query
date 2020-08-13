511fff3f34d14273ce042045fc6b92968e12c54e
adc work <adc9work@outlook.com>
2020-08-13 12:39:35 +0530

    Query

--- index.js
+++ index.js
@@ -6,9 +6,38 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts", (req, res) => {});
+const posts = {};
+//Posts Example
+// posts ===
+//   {
+//     "1": {
+//       id: "1",
+//       title: "post title",
+//       comments: [
+//         { id: "1", content: "comment!" },
+//         { id: "2", content: "comment!" },
+//       ],
+//     },
+//   };
 
-app.post("/events", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[i] = { id, title, comments: [] };
+  }
+  if (type === "CommentsCreated") {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    console.log(post);
+    post.comments.push({ id, content });
+  }
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("Listening on 4002");
