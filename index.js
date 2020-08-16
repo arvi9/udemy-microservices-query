ea505591b1918f05553ff4f562a9b5ae816f3bec
adc work <adc9work@outlook.com>
2020-08-16 23:03:02 +0530

    Query

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
-//Posts Example
-// posts ===
-//   {
-//     "1": {
-//       id: "1",
-//       title: "post title",
-//       comments: [
-//         { id: "1", content: "comment!" },
-//         { id: "2", content: "comment!" },
-//       ],
-//     },
-//   };
+
+const handleEvent = (type, data) => {
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
 
 app.get("/posts", (req, res) => {
   res.send(posts);
@@ -26,19 +42,24 @@ app.get("/posts", (req, res) => {
 
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
-  if (type === "PostCreated") {
-    const { id, title } = data;
-    posts[i] = { id, title, comments: [] };
-  }
-  if (type === "CommentsCreated") {
-    const { id, content, postId } = data;
-    const post = posts[postId];
-    console.log(post);
-    post.comments.push({ id, content });
-  }
+
+  handleEvent(type, data);
+
   res.send({});
 });
 
-app.listen(4002, () => {
-  console.log("Listening on 4002");
+app.listen(4002, async () => {
+  try {
+    console.log("Listening on 4002");
+
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (ex) {
+    console.log(ex);
+  }
 });
